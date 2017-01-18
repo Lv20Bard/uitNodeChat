5305066844a80ad70819668439569e638f98d6dc
Ben MacNeil <BenMacneil@Bens-Air.eastlink.ca>
2017-01-18 10:03:19 -0400

    Finish project

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ var bodyParser = require('body-parser');
 // your default messages and users array: this is where you'll store your messages and users
 var messages = [];
 var users = [];
+var connected = [];
 
 // set up to accept json as parameters
 app.use(bodyParser.json());
@@ -24,19 +25,58 @@ app.use('/css', express.static(path.join(__dirname, 'public/css')));
 // routes via express
 app.get('/', function(req, res) {
 
-	// @TODO: add your ejs rendering code here! Remember, this has been done many times
-	//		  before by many other people, so Google is your friend!
+	var tagline = "Welcome to the chat my dudes"
+
+	// Render the index
+	res.render('index',{
+		tagline: tagline
+	});
+
+
 });
 
 
 // socket.io functionality
 io.on('connection', function(socket){
-  console.log('Socket user connected!');
 
-  // @TODO: set up your send message, show messages and add username functions.
-  //		Remember, there is a video on all of this stuff that we watched!
+	connected.push(socket);
+	console.log('User Connected! %s Users connected!', connected.length);
+	
+
+	//disconnect
+	socket.on('disconnect',function(data){
+		users.splice(users.indexOf(socket.username), 1);
+		updateUsernames();
+		
+		connected.splice(connected.indexOf(socket), 1);
+		console.log('User Disconnected! %s Users connected', connected.length);
+		
+	
+	});
+
+	// Send message
+	socket.on('send message', function(data){
+		io.emit('new message', {msg: data, user:socket.username});
+	});
+
+
+
+	//add user
+	socket.on('new users', function(data, callback){
+		callback(true);
+		socket.username = data;
+		users.push(socket.username);
+		updateUsernames();
+	});
+
+	function updateUsernames(){
+		io.emit('get users', users);
+	};
+
 
 });
+
+
 
 http.listen(8080);
 console.log("Listening on port 8080...");
